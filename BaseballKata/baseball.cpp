59da5d2e307df87d855f2d829be808bb16ac155d
quokkaHunter <ekdlemdjq456@naver.com>
2024-05-17 14:55:17 +0900

    [Feature] Complete to develop Baseball Game

--- BaseballKata/baseball.cpp
+++ BaseballKata/baseball.cpp
@@ -14,23 +14,49 @@ class Baseball
 {
 public:
 	Baseball(string answer) { this->answer = answer; }
+
 	GuessResults& guess(string query)
 	{
 		assertIllegalArgument(query);
 
 		GuessResults guessResults = {};
 
+		checkStrike(query, guessResults);
+		checkBall(query, guessResults);
+		checkSolved(guessResults);
+
+		return guessResults;
+	}
+
+	void checkSolved(GuessResults& guessResults)
+	{
+		if (guessResults.strikes == 3)
+			guessResults.solved = true;
+	}
+
+	void checkBall(std::string& query, GuessResults& guessResults)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if (i == j) continue;
+
+				if (query[i] == answer[j])
+					guessResults.balls++;
+			}
+		}
+	}
+
+	void checkStrike(std::string& query, GuessResults& guessResults)
+	{
 		for (int i = 0; i < 3; i++)
 		{
 			if (query[i] == answer[i])
 				guessResults.strikes++;
 		}
-
-		if (guessResults.strikes == 3)
-			guessResults.solved = true;
-
-		return guessResults;
 	}
+
 	void assertIllegalArgument(std::string& query)
 	{
 		if (query.length() != 3)
@@ -48,10 +74,6 @@ public:
 	bool isOverlappedNumber(std::string& query)
 	{
 		return query[0] == query[1] || query[1] == query[2] || query[0] == query[2];
-	}
-	void setAnswer(string answer)
-	{
-		this->answer = answer;
 	}
 private:
 	string answer;
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -26,9 +26,33 @@ TEST_F(BaseballFixture, ThrowExceptionInvalidCase) {
 	assertIllegalArgument("121");
 }
 TEST_F(BaseballFixture, BaseballGameSolved) {
-	baseball.setAnswer("123");
 	GuessResults guessResults = baseball.guess("123");
 	EXPECT_TRUE(guessResults.solved);
 	EXPECT_EQ(3, guessResults.strikes);
+	EXPECT_EQ(0, guessResults.balls);
+}
+TEST_F(BaseballFixture, BaseballGameTwoStrikeZeroBall) {
+	GuessResults guessResults = baseball.guess("143");
+	EXPECT_EQ(2, guessResults.strikes);
+	EXPECT_EQ(0, guessResults.balls);
+}
+TEST_F(BaseballFixture, BaseballGameOneStrikeTwoBall) {
+	GuessResults guessResults = baseball.guess("213");
+	EXPECT_EQ(1, guessResults.strikes);
+	EXPECT_EQ(2, guessResults.balls);
+}
+TEST_F(BaseballFixture, BaseballGameZeroStrikeThreeBall) {
+	GuessResults guessResults = baseball.guess("312");
+	EXPECT_EQ(0, guessResults.strikes);
+	EXPECT_EQ(3, guessResults.balls);
+}
+TEST_F(BaseballFixture, BaseballGameZeroStrikeTwoBall) {
+	GuessResults guessResults = baseball.guess("342");
+	EXPECT_EQ(0, guessResults.strikes);
+	EXPECT_EQ(2, guessResults.balls);
+}
+TEST_F(BaseballFixture, BaseballGameZeroStrikeZeroBall) {
+	GuessResults guessResults = baseball.guess("456");
+	EXPECT_EQ(0, guessResults.strikes);
 	EXPECT_EQ(0, guessResults.balls);
 }
