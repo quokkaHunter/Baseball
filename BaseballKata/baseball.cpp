ff3d06f9b50de0e4ebf039052cddcedbcd2509d5
quokkaHunter <ekdlemdjq456@naver.com>
2024-05-17 13:56:39 +0900

    [Feature] Page 27

--- BaseballKata/baseball.cpp
+++ BaseballKata/baseball.cpp
@@ -8,6 +8,11 @@ class Baseball
 public:
 	string guess(string query)
 	{
+		if (query.length() != 3)
+		{
+			throw length_error("Length Error! Query Length should be 3");
+		}
+
 		string result;
 		return result;
 	}
