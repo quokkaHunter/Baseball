96f75fbec3ce652fc720baf9a546bbd0eca3b8da
quokkaHunter <ekdlemdjq456@naver.com>
2024-05-17 13:55:48 +0900

    [Feature] Page 25

--- BaseballKata/baseball.cpp
+++ BaseballKata/baseball.cpp
@@ -1,1 +1,14 @@
-﻿
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Baseball
+{
+public:
+	string guess(string query)
+	{
+		string result;
+		return result;
+	}
+};
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -2,6 +2,6 @@
 #include "../BaseballKata/baseball.cpp"
 
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	Baseball baseball;
+	EXPECT_THROW(baseball.guess("12"), length_error);
 }
